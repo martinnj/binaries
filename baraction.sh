e31667d8d58d82347d65e91657ecbf7cd114a42c
Martin Jørgensen <martinnjorgensen@gmail.com>
2014-02-27 15:43:37 +0100

    It hangs...

--- baraction.sh
+++ baraction.sh
@@ -6,9 +6,9 @@ SLEEP_SEC=5  # set bar_delay = 5 in /etc/spectrwm.conf
 #loops forever outputting a line every SLEEP_SEC secs
 i3status | while :
 do
-	read line
-        DB=`dropbox status`
-        MC=`cat /tmp/mc.fifo`
-        echo "MC:$MC | DB:$DB | $line"
-        sleep $SLEEP_SEC
+    read line
+    DB=`dropbox status`
+    #MC=`cat /tmp/mc.fifo` #Fix this, it hangs :/
+    echo "MC:$MC | DB:$DB | $line"
+    sleep $SLEEP_SEC
 done
