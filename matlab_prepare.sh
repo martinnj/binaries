f58b9f9552693d7b24bccf1416cf00b71725e488
Martin Jørgensen <martinnjorgensen@gmail.com>
2014-03-06 16:07:36 +0100

    Fixes issue where matlab hostid keeps changing.

--- matlab_prepare.sh
+++ matlab_prepare.sh
@@ -2,4 +2,4 @@
 sudo modprobe dummy
 sudo ip link set dummy0 down
 sudo ip l set dev dummy0 name eth0
-#sudo ip link set dev eth0 address 00:00:00:00:00:00
+sudo ip link set dev eth0 address ea:44:e7:2d:7d:dc
