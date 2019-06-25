d887fa263e587066d6f07f19810561dbb97a4671
Martin Jørgensen <hello@martinnj.dk>
2019-06-25 07:41:28 +0200

    Spring cleaning.

--- matlab_prepare.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-sudo modprobe dummy
-sudo ip link set dummy0 down
-sudo ip l set dev dummy0 name eth0
-sudo ip link set dev eth0 address ea:44:e7:2d:7d:dc
--- mountGpuCluster.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo modprobe fuse
-sudo mkdir /mnt/sshgpu
-sudo chown martin /mnt/sshgpu
-sshfs tzk173@gpu03-diku-apl: /mnt/sshgpu
